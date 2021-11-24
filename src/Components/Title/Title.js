5093474f3a18e2041bd545de0a14e71725933a23
Lucas <lucasmoauro.93@gmail.com>
2021-11-23 22:00:58 -0300

    OT103-48 Refactorizacion de codigo

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -4,11 +4,7 @@ import defaultImage from '../../assets/images/404.png'
 export const Title = ({ title, image }) => {
   return (
     <div>
-      {image === undefined ? (
-        <img src={ defaultImage } alt="..." />
-      ) : (
-        <img src={image.default} alt="..." />
-      )}
+     <img src={ !image ? defaultImage : image.default } alt="" />
       <h1 style={{textAlign: "center"}}>{title}</h1>
     </div>
   );

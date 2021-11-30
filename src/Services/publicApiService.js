270d828b220e45dc61aeeb7c541d0141fe0df1bc
Luis Ramiro Krupoviesa <krupoviesaramiro@gmail.com>
2021-11-30 10:50:39 -0300

    add reusable Post for public endpoints (#28)
    Co-authored-by: FlaviaCohen <flaviacohen1988@gmail.com>

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,7 +2,21 @@ import axios from "axios";
 
 const baseUrl = "http://ongapi.alkemy.org/api";
 
-const Get = async(category, id) => {
+export const Post = async (endPoint, body) => {
+  const url = `${baseUrl}/${endPoint}`;
+
+  try {
+    const response = await axios.post(url, body);
+
+    return response.data;
+  } catch (error) {
+    return { success: false, error };
+  }
+};
+
+
+
+export const Get = async(category, id) => {
     const url = id ? `${baseUrl}/${category}/${id}` : `${baseUrl}/${category}`;
     try {
         const response = await axios.get(url);

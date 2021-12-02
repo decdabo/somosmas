1c3ff8930618c3fe67d40d35e15d7cc476b3bdd9
Nahuel De Fazio <73983784+nahueldefazio@users.noreply.github.com>
2021-12-02 11:28:19 -0300

    Implemente el metodo Get en el componente SlidesForm.js (#42)

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import axios from "axios";
+
 import { useFormik } from "formik";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
@@ -10,122 +10,120 @@ import { slidesPost, slidesPatch } from "./helpers/slidesAPI";
 
 import "../FormStyles.css";
 import "./slidesForm.scss";
+import { Get } from "../../Services/privateApiService";
 
-const SlidesForm = ({ data }) => {
-  const url = "http://ongapi.alkemy.org/api";
+const SlidesForm = ({data}) => {
 
-  const [slides, setslides] = useState([]);
-  const [imagePreview, setImagePreview] = useState("");
+    const [slides, setslides] = useState([]);
+    const [imagePreview, setImagePreview] = useState("");
 
-  const image = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setImagePreview(reader.result);
+    const image = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setImagePreview(reader.result);
+        };
     };
-  };
 
-  useEffect(() => {
-    axios.get(`${url}/slides`).then(({ data }) => {
-      setslides(data.data);
+    useEffect(() => {
+        Get("slides").then(r => setslides(r))
+    }, []);
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            description: "",
+            order: "",
+            image: "",
+        },
+        validate,
+        onSubmit: (values) => {
+            if (slides.findIndex((x) => x.id === Number(values.order)) > 0) {
+                formik.setFieldError("order", "El order debe ser unico.");
+            } else {
+                if (!data) {
+                    slidesPost(formik);
+                    formik.resetForm();
+                } else {
+                    slidesPatch(formik);
+                }
+            }
+        },
     });
-  }, []);
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      description: "",
-      order: "",
-      image: "",
-    },
-    validate,
-    onSubmit: (values) => {
-      if (slides.findIndex((x) => x.id === Number(values.order)) > 0) {
-        formik.setFieldError("order", "El order debe ser unico.");
-      } else {
-        if (!data) {
-          slidesPost(formik);
-          formik.resetForm();
-        } else {
-          slidesPatch(formik);
-        }
-      }
-    },
-  });
+    return (
+        <form className="form-container" onSubmit={ formik.handleSubmit }>
+            <label htmlFor="name">Nombre:</label>
+            <input
+                id="name"
+                className="input-field"
+                type="text"
+                name="name"
+                onChange={ formik.handleChange }
+                onBlur={ formik.handleBlur }
+                value={ formik.values.name || data?.name || "" }
+                placeholder="Slide Title"
+                autoComplete="off"
+            />
 
-  return (
-    <form className="form-container" onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Nombre:</label>
-      <input
-        id="name"
-        className="input-field"
-        type="text"
-        name="name"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.name || data?.name || ""}
-        placeholder="Slide Title"
-        autoComplete="off"
-      />
-
-      {formik.touched.name && formik.errors.name ? (
-        <div className="error-msg">{formik.errors.name}</div>
-      ) : null}
-      <label htmlFor="description">Descripcion:</label>
-      <CKEditor
-        className="form__input"
-        id="description"
-        editor={ClassicEditor}
-        data={data?.description || formik.values.description || ""}
-        onChange={(e, editor) => {
-          formik.setFieldValue("description", editor.getData());
-        }}
-      />
-      {formik.touched.description && formik.errors.description ? (
-        <div className="error-msg">{formik.errors.description}</div>
-      ) : null}
-      <label htmlFor="order">Orden:</label>
-      <input
-        className="input-field"
-        type="text"
-        name="order"
-        id="order"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.order || data?.order || ""}
-        placeholder="0"
-        autoComplete="off"
-      />
-      {formik.touched.order && formik.errors.order ? (
-        <div className="error-msg">{formik.errors.order}</div>
-      ) : null}
-      <label htmlFor="image">Imagen:</label>
-      <input
-        className="input-field"
-        type="file"
-        id="image"
-        name="image"
-        onChange={(e) => {
-          formik.handleChange(e);
-          image(e);
-        }}
-        onBlur={formik.handleBlur}
-        value={formik.values.image || data?.image || ""}
-        placeholder="Write the description"
-        autoComplete="off"
-      />
-      {imagePreview ? (
-        <img src={imagePreview} className="image-preview" alt="..." />
-      ) : null}
-      {formik.touched.image && formik.errors.image ? (
-        <div className="error-msg">{formik.errors.image}</div>
-      ) : null}
-      <button className="submit-btn" type="submit">
-        Send
-      </button>
-    </form>
-  );
+            { formik.touched.name && formik.errors.name ? (
+                <div className="error-msg">{ formik.errors.name }</div>
+            ) : null }
+            <label htmlFor="description">Descripcion:</label>
+            <CKEditor
+                className="form__input"
+                id="description"
+                editor={ ClassicEditor }
+                data={ data?.description || formik.values.description || "" }
+                onChange={ (e, editor) => {
+                    formik.setFieldValue("description", editor.getData());
+                } }
+            />
+            { formik.touched.description && formik.errors.description ? (
+                <div className="error-msg">{ formik.errors.description }</div>
+            ) : null }
+            <label htmlFor="order">Orden:</label>
+            <input
+                className="input-field"
+                type="text"
+                name="order"
+                id="order"
+                onChange={ formik.handleChange }
+                onBlur={ formik.handleBlur }
+                value={ formik.values.order || data?.order || "" }
+                placeholder="0"
+                autoComplete="off"
+            />
+            { formik.touched.order && formik.errors.order ? (
+                <div className="error-msg">{ formik.errors.order }</div>
+            ) : null }
+            <label htmlFor="image">Imagen:</label>
+            <input
+                className="input-field"
+                type="file"
+                id="image"
+                name="image"
+                onChange={ (e) => {
+                    formik.handleChange(e);
+                    image(e);
+                } }
+                onBlur={ formik.handleBlur }
+                value={ formik.values.image || data?.image || "" }
+                placeholder="Write the description"
+                autoComplete="off"
+            />
+            { imagePreview ? (
+                <img src={ imagePreview } className="image-preview" alt="..."/>
+            ) : null }
+            { formik.touched.image && formik.errors.image ? (
+                <div className="error-msg">{ formik.errors.image }</div>
+            ) : null }
+            <button className="submit-btn" type="submit">
+                Send
+            </button>
+        </form>
+    );
 };
 
 export default SlidesForm;

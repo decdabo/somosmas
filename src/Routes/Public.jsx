7653b0fa9297c7c72f749103f28b5559850fd8c9
Lucas <lucasmoauro.93@gmail.com>
2021-12-18 16:56:23 -0300

    [OT103-129] Feat (#134)
    * remove: redux boilerplate and redux
    
    * done: functional component
    
    * api: modify category
    
    * working component
    
    * image to base64 helper
    
    * fix: image received from object
    
    * fix: returns message
    
    * component ready to refactor
    
    * testing errors, verify
    
    * component done, waiting for title component
    
    * component done, waiting for title component
    
    * 0T103-53(SlideHome)
    
    * 0T103-53(SlideHome)
    
    * conficlts resolve
    
    * component refactor
    
    * corrections
    
    * resolve
    
    * irrelevant changes
    
    * libs: cypress and jest
    
    * fix: ckeditor bugs
    
    * refactor done
    
    * merge: OT103-28
    
    * Components created
    
    * fix: Changed anchor to Link
    
    * CKeditor added
    
    * Fix: bug in img logic
    
    * no
    
    * libs: react-icons
    
    * spinner created
    
    * Feature: info button and spinner
    
    * res
    
    * slide class
    
    * deleting import app
    
    * console.logs removed
    
    * added exact router path
    
    * comments removed
    
    * delete backoffice folder
    
    * ahora si merge
    
    * Validations added
    
    * Form validations added
    
    * title disabled by the moment
    
    * Route created
    
    * Reusable form and code modularization
    
    * Removed console log and inline style
    
    * Removed unnecesary logic
    
    * Inline style removed
    
    * Routes and validations
    
    * CKEditor added, order validation and Img preview
    
    * Conflicts resolved
    
    * type error remove
    
    * Route and Users component created
    
    * Code modularization and styles added
    
    * get service
    
    * screen
    
    * App.js deleted and json created for mocking API data
    
    * OT103-64
    
    * changes
    
    * Last changes
    
    * changes in the app.jsx
    
    * Service get usuarios
    
    * expansión de servicios
    
    * i forgot things
    
    * canges in styles table
    
    * table practices
    
    * tableStyles add
    
    * tableStyles
    
    * Styles fixed
    
    * New form styles added
    
    * Conflicts resolved
    
    * Removed unused attributes
    
    * env and example env created
    
    * env example changed
    
    * Creacion de Slice
    
    * Changed reducer name
    
    * Loader component added
    
    * Conflicts resolved and alert component added
    
    * Alerts component  added
    
    * change in slice to receive an id and GET request replaced
    
    * Terms placeholder, Popup and form logic for terms acceptance
    
    * Conflicts resolved
    
    * Fixed logic
    
    * Added images for school slider and responsive
    
    * Breakpoints variables
    
    * Lazy load for routes and routes modularization
    
    Co-authored-by: Julian Kominovic <juliankominovic@gmail.com>
    Co-authored-by: decdabo <david.caro.dec@hotmail.com>

--- src/Routes/Public.jsx
+++ src/Routes/Public.jsx
@@ -1,68 +1,74 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { AnimatedSwitch } from "react-router-transition";
+
+import LoadingSpinner from "../Components/Spinner/LoadingSpinner";
 import { mapStyles } from "../helpers/routerTransitions";
-import ActivitiesForm from "../Components/Activities/ActivitiesForm";
-import CategoriesForm from "../Components/Categories/CategoriesForm";
-import NewsForm from "../Components/News/NewsForm";
-import TestimonialForm from "../Components/Testimonials/TestimonialsForm";
-import UserForm from "../Components/Users/UsersForm";
-import SchoolCampaign from "../Campaigns/School/SchoolCampaign";
-import ToysCampaign from "../Campaigns/Toys/ToysCampaign";
-import MembersForm from "../Components/Members/MembersForm";
-import ProjectsForm from "../Components/Projects/ProjectsForm";
-import { AboutMain } from "../Components/About/AboutMain";
-import ContactForm from "../Components/Contact/ContactForm";
-import RegisterForm from "../Components/Auth/RegisterForm";
-import LoginForm from "../Components/Auth/LoginForm";
-import ActivitiesList from "../Components/Activities/ActivitiesList";
-import ActivityInfo from "../Components/Activities/Detail/ActivityInfo";
-import { DonationsGreet } from "../Components/Donations/DonationsGreet";
-import { ThanksGreet } from "../Components/Donations/ThanksGreet";
-import Members from "../Components/About/Members";
-import Home from "../Home/Home";
-import Contact from "../Components/Contact/Contact";
-import PageNotFound from "../Components/PageNotFound/PageNotFound";
+import {
+	AboutMain,
+	ActivitiesForm,
+	ActivitiesList,
+	ActivityInfo,
+	CategoriesForm,
+	Contact,
+	ContactForm,
+	DonationsGreet,
+	Home,
+	LoginForm,
+	Members,
+	MembersForm,
+	NewsForm,
+	PageNotFound,
+	ProjectsForm,
+	RegisterForm,
+	SchoolCampaign,
+	TestimonialForm,
+	ThanksGreet,
+	ToysCampaign,
+	UserForm,
+} from "./Router/publicRoutes";
 
 function Public() {
 	return (
 		<>
 			<BrowserRouter>
-				<AnimatedSwitch
-					atEnter={{ opacity: 0 }}
-					atLeave={{ opacity: 0 }}
-					atActive={{ opacity: 1 }}
-					className="switch-wrapper"
-					mapStyles={mapStyles}
-				>
-					<Route exact path="/create-activity" component={ActivitiesForm} />
-					<Route exact path="/create-category" component={CategoriesForm} />
-					<Route exact path="/create-news" component={NewsForm} />
-					<Route
-						exact
-						path="/create-testimonials"
-						component={TestimonialForm}
-					/>
-					<Route exact path="/create-user" component={UserForm} />
-					<Route exact path="/edit-user/:id" component={UserForm} />
-					<Route exact path="/school-campaign" component={SchoolCampaign} />
-					<Route exact path="/toys-campaign" component={ToysCampaign} />
-					<Route exact path="/create-member" component={MembersForm} />
-					<Route exact path="/create-project" component={ProjectsForm} />
-					<Route exact path="/about" component={AboutMain} />
-					<Route exact path="/contact-form" component={ContactForm} />
-					<Route exact path="/register-form" component={RegisterForm} />
-					<Route exact path="/login-form" component={LoginForm} />
-					<Route exact path="/actividades" component={ActivitiesList} />
-					<Route exact path="/actividades/:id" component={ActivityInfo} />
-					<Route exact path="/donar" component={DonationsGreet} />
-					<Route exact path="/gracias" component={ThanksGreet} />
-					<Route exact path="/about/members" component={Members} />
-					<Route exact path="/contacto" component={Contact} />
+				<Suspense fallback={<LoadingSpinner />}>
+					<AnimatedSwitch
+						atEnter={{ opacity: 0 }}
+						atLeave={{ opacity: 0 }}
+						atActive={{ opacity: 1 }}
+						className="switch-wrapper"
+						mapStyles={mapStyles}
+					>
+						<Route exact path="/create-activity" component={ActivitiesForm} />
+						<Route exact path="/create-category" component={CategoriesForm} />
+						<Route exact path="/create-news" component={NewsForm} />
+						<Route
+							exact
+							path="/create-testimonials"
+							component={TestimonialForm}
+						/>
+						<Route exact path="/create-user" component={UserForm} />
+						<Route exact path="/edit-user/:id" component={UserForm} />
+						<Route exact path="/school-campaign" component={SchoolCampaign} />
+						<Route exact path="/toys-campaign" component={ToysCampaign} />
+						<Route exact path="/create-member" component={MembersForm} />
+						<Route exact path="/create-project" component={ProjectsForm} />
+						<Route exact path="/about" component={AboutMain} />
+						<Route exact path="/contact-form" component={ContactForm} />
+						<Route exact path="/register-form" component={RegisterForm} />
+						<Route exact path="/login-form" component={LoginForm} />
+						<Route exact path="/actividades" component={ActivitiesList} />
+						<Route exact path="/actividades/:id" component={ActivityInfo} />
+						<Route exact path="/donar" component={DonationsGreet} />
+						<Route exact path="/gracias" component={ThanksGreet} />
+						<Route exact path="/about/members" component={Members} />
+						<Route exact path="/contacto" component={Contact} />
 
-					<Route exact path="/" component={Home} />
-					<Route component={PageNotFound} />
-				</AnimatedSwitch>
+						<Route exact path="/" component={Home} />
+						<Route component={PageNotFound} />
+					</AnimatedSwitch>
+				</Suspense>
 			</BrowserRouter>
 		</>
 	);
---
+++ src/Routes/Router/publicRoutes.jsx
@@ -0,0 +1,73 @@
+import { lazy } from "react";
+
+export const ActivitiesForm = lazy(() =>
+	import("../../Components/Activities/ActivitiesForm")
+);
+
+export const CategoriesForm = lazy(() =>
+	import("../../Components/Categories/CategoriesForm")
+);
+
+export const NewsForm = lazy(() => import("../../Components/News/NewsForm"));
+
+export const TestimonialForm = lazy(() =>
+	import("../../Components/Testimonials/TestimonialsForm")
+);
+
+export const UserForm = lazy(() => import("../../Components/Users/UsersForm"));
+
+export const SchoolCampaign = lazy(() =>
+	import("../../Campaigns/School/SchoolCampaign")
+);
+
+export const ToysCampaign = lazy(() =>
+	import("../../Campaigns/Toys/ToysCampaign")
+);
+
+export const MembersForm = lazy(() =>
+	import("../../Components/Members/MembersForm")
+);
+
+export const ProjectsForm = lazy(() =>
+	import("../../Components/Projects/ProjectsForm")
+);
+
+export const { AboutMain } = lazy(() =>
+	import("../../Components/About/AboutMain")
+);
+
+export const ContactForm = lazy(() =>
+	import("../../Components/Contact/ContactForm")
+);
+
+export const RegisterForm = lazy(() =>
+	import("../../Components/Auth/RegisterForm")
+);
+
+export const LoginForm = lazy(() => import("../../Components/Auth/LoginForm"));
+
+export const ActivitiesList = lazy(() =>
+	import("../../Components/Activities/ActivitiesList")
+);
+
+export const ActivityInfo = lazy(() =>
+	import("../../Components/Activities/Detail/ActivityInfo")
+);
+
+export const { DonationsGreet } = lazy(() =>
+	import("../../Components/Donations/DonationsGreet")
+);
+
+export const { ThanksGreet } = lazy(() =>
+	import("../../Components/Donations/ThanksGreet")
+);
+
+export const Members = lazy(() => import("../../Components/About/Members"));
+
+export const Home = lazy(() => import("../../Home/Home"));
+
+export const Contact = lazy(() => import("../../Components/Contact/Contact"));
+
+export const PageNotFound = lazy(() =>
+	import("../../Components/PageNotFound/PageNotFound")
+);

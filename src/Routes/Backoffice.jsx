8a2d46a8e965f09c6a80a1dbb08e94c9e3cecaf2
Luis Ramiro Krupoviesa <krupoviesaramiro@gmail.com>
2021-12-21 23:44:44 -0300

    fix router (#140)

--- src/Routes/Backoffice.jsx
+++ src/Routes/Backoffice.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { AnimatedSwitch } from "react-router-transition";
 import { mapStyles } from "../helpers/routerTransitions";
 import SlidesForm from "../Components/Slides/SlidesForm";
@@ -13,57 +13,43 @@ import { ScreenSliderList } from "../backoffice/SlidesScreen/ScreenSliderList";
 import HomeDashboard from "../Components/HomeDashboard/HomeDashboard";
 import OrganizationData from "../backoffice/Organization/OrganizationData";
 import LayoutBackoffice from "../backoffice/Layouts/LayoutBackoffice";
+import PageNotFound from "../Components/PageNotFound/PageNotFound";
 
 function Backoffice() {
 	return (
-		<>
-			<BrowserRouter>
-				<AnimatedSwitch
-					atEnter={{ opacity: 0 }}
-					atLeave={{ opacity: 0 }}
-					atActive={{ opacity: 1 }}
-					className="switch-wrapper"
-					mapStyles={mapStyles}
-				>
-					<LayoutBackoffice>
-						<Route
-							exact
-							path="/backoffice/Slides/create"
-							component={SlidesForm}
-						/>
-						<Route exact path="/backoffice/home" component={HomeForm} />
-						<Route
-							exact
-							path="/backoffice/members/edit"
-							component={MembersEdit}
-						/>
-						<Route
-							exact
-							path="/backoffice/organization/edit"
-							component={OrganizationForm}
-						/>
-						<Route
-							exact
-							path="/backoffice/activities"
-							component={ManageActivities}
-						/>
-						<Route exact path="/backoffice/users" component={UsersList} />
-						<Route exact path="/backoffice/news" component={NewsList} />
-						<Route
-							exact
-							path="/backoffice/Slides"
-							component={ScreenSliderList}
-						/>
-						<Route
-							exact
-							path="/backoffice/organization"
-							component={OrganizationData}
-						/>
-						<Route path="/backoffice" exact component={HomeDashboard} />
-					</LayoutBackoffice>
-				</AnimatedSwitch>
-			</BrowserRouter>
-		</>
+		<LayoutBackoffice>
+			<AnimatedSwitch
+				atEnter={{ opacity: 0 }}
+				atLeave={{ opacity: 0 }}
+				atActive={{ opacity: 1 }}
+				className="switch-wrapper"
+				mapStyles={mapStyles}
+			>
+				<Route exact path="/backoffice/Slides/create" component={SlidesForm} />
+				<Route exact path="/backoffice/home" component={HomeForm} />
+				<Route exact path="/backoffice/members/edit" component={MembersEdit} />
+				<Route
+					exact
+					path="/backoffice/organization/edit"
+					component={OrganizationForm}
+				/>
+				<Route
+					exact
+					path="/backoffice/activities"
+					component={ManageActivities}
+				/>
+				<Route exact path="/backoffice/users" component={UsersList} />
+				<Route exact path="/backoffice/news" component={NewsList} />
+				<Route exact path="/backoffice/Slides" component={ScreenSliderList} />
+				<Route
+					exact
+					path="/backoffice/organization"
+					component={OrganizationData}
+				/>
+				<Route path="/backoffice" exact component={HomeDashboard} />
+				<Route component={PageNotFound} />
+			</AnimatedSwitch>
+		</LayoutBackoffice>
 	);
 }
 export default Backoffice;

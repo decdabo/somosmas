2b967202dab4f95a85feff81a9d93d2aa59e3c87
David Caro <david.caro.dec@hotmail.com>
2021-12-23 05:31:04 -0300

    merge bug

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import Menu from '../../assets/images/menu.svg'
-import '../Header/header.scss'
-import { SideBar } from './SideBar';
-=======
 import React, { useState } from "react";
 import Menu from "../../assets/images/menu.svg";
 import "../Header/header.scss";
 import { SideBar } from "./SideBar";
->>>>>>> 2c8ac5f9539943d787bb31efeeb2a930df6f9a59
 
 
 

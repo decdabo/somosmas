f2c37a3a0c97be572e77ba460f319b0aa5c8f40c
jkominovic <70329467+JulianKominovic@users.noreply.github.com>
2021-12-26 15:07:24 -0300

    AJUSTE - Login-form (#160)
    * remove: redux boilerplate and redux
    
    * done: functional component
    
    * api: modify category
    
    * working component
    
    * image to base64 helper
    
    * fix: image received from object
    
    * fix: returns message
    
    * component ready to refactor
    
    * testing errors, verify
    
    * component done, waiting for title component
    
    * component done, waiting for title component
    
    * component refactor
    
    * libs: cypress and jest
    
    * fix: ckeditor bugs
    
    * refactor done
    
    * merge: OT103-28
    
    * Fix: bug in img logic
    
    * Console.log and tests removed
    
    * Console.log removed
    
    * libs: react-icons
    
    * spinner created
    
    * Feature: info button and spinner
    
    * console.logs removed
    
    * added exact router path
    
    * comments removed
    
    * created getActivityById from endpoint /activities/:id
    
    * added activities/:id route
    
    * added activity info component
    
    * documentation done
    
    * flex fixed
    
    * errors fixed
    
    * title disabled by the moment
    
    * main pulled
    
    * component NewsItemList done
    
    * route /backoffice/news  modified
    
    * component done, waiting for remove action
    
    * created news list
    
    * removed todo annotation
    
    * added styles
    
    * styles applied
    
    * styles created and applied
    
    * Delete yarn.lock
    
    * styles done and applied
    
    * catogories form styled
    
    * conflicting files commented
    
    * redux, redux-toolkit and redux-thunk installed
    
    * store created
    
    * conflict resolved
    
    * provider added
    
    * dev tool enabled
    
    * redux setup
    
    * Delete Activities.scss
    
    * react-redux added
    
    * reducers added to index
    
    * validateAuth created
    
    * authSlice created
    
    * authActions custom hooks created
    
    * dotenv package installed
    
    * removed dotenv
    
    * Deleted my previous styles file
    
    * Styles adjusted
    
    * Components adjusted
    
    * Mock componente deleted
    
    * utils changes
    
    * alerts added
    
    * skeletons added
    
    * little response fix
    
    * contact get alert error
    
    * contact form post alert error
    
    * Skeletons changed. Custom activity skeleton made
    
    * documentation activity skeleton
    
    * name fixed
    
    * timeout added
    
    * activity details centered
    
    * skeleton fixed
    
    * Added helper function
    
    * refactor
    
    * Merge main
    
    * auth features
    
    * ckeditor global fix
    
    * Login fixed
    
    * login form history push
    
    * Push state fixed
    
    Co-authored-by: Lucas <lucasmoauro.93@gmail.com>
    Co-authored-by: FlaviaCohen <flaviacohen1988@gmail.com>

--- src/Components/Auth/LoginForm.jsx
+++ src/Components/Auth/LoginForm.jsx
@@ -1,115 +1,124 @@
-import React, { useContext, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/components/formStyles.scss";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Redirect } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
 import useAuthActions from "../../store/hooks/useAuthActions";
+import { useHistory } from "react-router-dom";
 
 const LoginForm = () => {
 	const [initialValues, setInitialValues] = useState({
 		email: "",
 		password: "",
 	});
-
-	const { isLogged } = useAuthActions();
+	const history = useHistory();
 	const [formEnviado, setFormEnviado] = useState(false);
+	const { validateUserLogin, isLogged } = useAuthActions();
 
-	if (isLogged) {
-		return <Redirect to="/" />;
-	}
+	useEffect(() => {
+		if (isLogged) history.push({ pathname: "/" });
+		const timeout = setTimeout(() => {
+			setFormEnviado(false);
+		}, 2000);
+		return () => {
+			timeout;
+		};
+	}, [formEnviado]);
+
 	return (
-		<Formik
-			initialValues={{
-				email: "",
-				password: "",
-			}}
-			validate={(values) => {
-				let errores = {};
+		<>
+			<h1 className="title my-10px txt-center">Login</h1>
+			<Formik
+				initialValues={{
+					email: "",
+					password: "",
+				}}
+				validate={(values) => {
+					let errores = {};
 
-				if (!values.email.trim()) {
-					errores.email = "Please enter an email";
-				} else if (
-					!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)
-				) {
-					errores.email = "Please enter a valid email";
-				}
+					if (!values.email.trim()) {
+						errores.email = "Please enter an email";
+					} else if (
+						!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+							values.email
+						)
+					) {
+						errores.email = "Please enter a valid email";
+					}
 
-				if (!values.password.trim()) {
-					errores.password = "Please enter a password";
-				} else if (values.password.length < 6) {
-					errores.password = "Your password must be at least 6 characters";
-				} else if (values.password.search(/[a-z]/i) < 0) {
-					errores.password = "Your password must contain at least one letter.";
-				} else if (values.password.search(/[0-9]/) < 0) {
-					errores.password = "Your password must contain at least one digit.";
-				} else if (values.password.search(/(?=.*[!@#$%^&*])/)) {
-					errores.password =
-						"Your password must contain at least one special character.";
-				}
+					if (!values.password.trim()) {
+						errores.password = "Please enter a password";
+					} else if (values.password.length < 6) {
+						errores.password = "Your password must be at least 6 characters";
+					} else if (values.password.search(/[a-z]/i) < 0) {
+						errores.password =
+							"Your password must contain at least one letter.";
+					} else if (values.password.search(/[0-9]/) < 0) {
+						errores.password = "Your password must contain at least one digit.";
+					} else if (values.password.search(/(?=.*[!@#$%^&*])/)) {
+						errores.password =
+							"Your password must contain at least one special character.";
+					}
 
-				return errores;
-			}}
-			onSubmit={(values, { resetForm }) => {
-				setInitialValues({
-					email: values.email,
-					password: values.password,
-				});
-
-				setFormEnviado(true);
-
-				setTimeout(() => {
+					return errores;
+				}}
+				onSubmit={(values, { resetForm }) => {
 					setFormEnviado(false);
-				}, 10000);
-				alert(
-					`
-                    Email: ${values.email}
-                    Password: ${values.password}                    
-                    `
-				);
-				resetForm();
-			}}
-		>
-			{({ errors }) => (
-				<Form className="form__container">
-					<Field
-						className="form__input"
-						id="email"
-						type="email"
-						name="email"
-						placeholder="Enter email"
-					/>
+					validateUserLogin(values.email, values.password)
+						.then(() => {
+							if (isLogged) {
+								resetForm();
+								setInitialValues({
+									email: "",
+									password: "",
+								});
+							}
+							setFormEnviado(true);
+						})
+						.catch(() => {
+							setFormEnviado(true);
+						});
+				}}
+			>
+				{({ errors }) => (
+					<Form className="form__container">
+						<Field
+							className="form__input"
+							id="email"
+							type="email"
+							name="email"
+							placeholder="Enter email"
+						/>
 
-					<ErrorMessage
-						name="email"
-						component={() => (
-							<div className="form__message-fail">{errors.email}</div>
-						)}
-					/>
+						<div className="form__message-validation">{errors.email}</div>
 
-					<Field
-						className="form__input"
-						id="password"
-						type="password"
-						name="password"
-						placeholder="Enter password"
-					/>
+						<Field
+							className="form__input"
+							id="password"
+							type="password"
+							name="password"
+							placeholder="Enter password"
+						/>
 
-					<ErrorMessage
-						name="password"
-						component={() => (
-							<div className="form__message-fail">{errors.password}</div>
-						)}
-					/>
+						<div className="form__message-validation">{errors.password}</div>
 
-					<button className="form__btn-primary" type="submit">
-						Log In
-					</button>
+						<button className="form__btn-primary" type="submit">
+							Login
+						</button>
 
-					{formEnviado && (
-						<p className="form__message-success">Log in successfull</p>
-					)}
-				</Form>
-			)}
-		</Formik>
+						<div
+							className={
+								isLogged ? "form__message-success" : "form__message-fail"
+							}
+						>
+							{formEnviado
+								? isLogged
+									? "Login exitoso"
+									: "Usuario y/o contraseña incorrecto"
+								: null}
+						</div>
+					</Form>
+				)}
+			</Formik>
+		</>
 	);
 };
 
--- src/Components/Categories/InputCkEditor.jsx
+++ src/Components/Categories/InputCkEditor.jsx
@@ -2,38 +2,33 @@ import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import classicEditor from "@ckeditor/ckeditor5-build-classic";
 const InputCkEditor = ({
-  name,
-  required,
-  values,
-  defaultObjectValues,
-  setTouched,
-  touched,
-  errors,
+	name,
+	required,
+	values,
+	defaultObjectValues,
+	setTouched,
+	touched,
+	errors,
 }) => {
-  return (
-    <div>
-      <CKEditor
-        className="form__input"
-        name={name}
-        editor={classicEditor}
-        data={defaultObjectValues ? defaultObjectValues[name] : ""}
-        onChange={(e, editor) => (values[name] = editor.getData())}
-        onBlur={() => {
-          setTouched({ ...touched, [name]: true });
-        }}
-        config={{
-          placeholder: "Contenido",
-          cloudServices: {
-            tokenUrl:
-              "https://85122.cke-cs.com/token/dev/63f1e5122f7b89374a44f0ba134c7a670437bab84212188ac1b17d829d92",
-            uploadUrl: "https://85122.cke-cs.com/easyimage/upload/",
-          },
-        }}
-      />
+	return (
+		<div>
+			<CKEditor
+				className="form__input"
+				name={name}
+				editor={classicEditor}
+				data={defaultObjectValues ? defaultObjectValues[name] : ""}
+				onChange={(e, editor) => (values[name] = editor.getData())}
+				onBlur={() => {
+					setTouched({ ...touched, [name]: true });
+				}}
+				config={{
+					placeholder: "Contenido",
+				}}
+			/>
 
-      <div className="form__message-validation">{errors[name]}</div>
-    </div>
-  );
+			<div className="form__message-validation">{errors[name]}</div>
+		</div>
+	);
 };
 
 export default InputCkEditor;

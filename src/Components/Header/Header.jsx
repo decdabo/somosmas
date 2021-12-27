6a55f497597b5c8d88854fb4dfe151195139928f
Fausto Rossi <76191699+FaustoRossi@users.noreply.github.com>
2021-12-27 11:20:33 -0300

    [OT103-142] Feat (#163)
    * first commit, card 35
    
    * resolvng other conflicts
    
    * more conflicts
    
    * fixes
    
    * conflict resolve
    
    * valid linkedIn url fix
    
    * scss solved
    
    * problems fix
    
    * conflicts fixed
    
    * fix
    
    * fx form
    
    * button placed
    
    * button style
    
    * donate button
    
    * routes fix
    
    * icons added
    
    * changes added
    
    Co-authored-by: Fausto Rossi <faustorossi@usal.edu.ar>

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Menu from '../../assets/images/menu.svg'
-import '../Header/header.scss'
-import { SideBar } from './SideBar';
+import React, { useState } from "react";
+import Menu from "../../assets/images/menu.svg";
+import "../Header/header.scss";
+import { SideBar } from "./SideBar";
 
 const Header = () => {
 	const [open, setOpen] = useState(false);
@@ -10,7 +10,7 @@ const Header = () => {
 			<div className="headerLogo">
 				<button className="headerLogo__btn" onClick={() => setOpen(!open)}>
 					<img src={Menu} alt="Menu" className="headerLogo__img" />
-				</button>n
+				</button>
 			</div>
 			<SideBar isOpen={open} />
 		</>

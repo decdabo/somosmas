627948f0f3acaf5ba0dc44612e2ace226b4c136f
Luis Ramiro Krupoviesa <krupoviesaramiro@gmail.com>
2021-12-27 15:21:41 -0300

    AJUSTE - Header web publica (#162)
    * fix header styles
    
    * fix login button
    
    * fix style
    
    * delete unused line

--- src/Components/Header/AuthLogin.js
+++ src/Components/Header/AuthLogin.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const AuthLogin = () => {
-    return (
-        <>
-            <li className="nav-item mb-2 mb-lg-0 mb-lg-0">
-                <NavLink className="nav-link me-3 mx-lg-5" to="/login-form" exact >
-                    Login
-                </NavLink>
-            </li>
-            <li className="nav-item mb-2 mb-lg-0 mb-lg-0">
-                <NavLink className="nav-link me-3 me-lg-5" to="/register-form" exact >
-                    Registrate
-                </NavLink>
-            </li>
-        </>
-    )
-}
+	return (
+		<>
+			<NavLink to="/login-form" exact>
+				<button className="form__btn-tertiary">Login</button>
+			</NavLink>
+			<NavLink to="/register-form" exact>
+				<button className="form__btn-secondary">Registrarse</button>
+			</NavLink>
+		</>
+	);
+};
--- src/Components/Header/AuthLogout.js
+++ src/Components/Header/AuthLogout.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { logout } from '../../store/slices/authSlice';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { logout } from "../../store/slices/authSlice";
 
 export const AuthLogout = () => {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-    const handleLogout = () => {
-        dispatch(logout())
-        localStorage.removeItem('token');
-    }
+	const handleLogout = () => {
+		dispatch(logout());
+		localStorage.removeItem("token");
+	};
 
-    return (
-        <li className="nav-item mb-2 mb-lg-0">
-            <NavLink onClick={handleLogout} className="nav-link me-3 mx-lg-5" to="/" exact >
-                Logout
-            </NavLink>
-        </li>
-    )
-}
+	return (
+		<NavLink onClick={handleLogout} to="/" exact>
+			<button className="form__btn-secondary">Logout</button>
+		</NavLink>
+	);
+};

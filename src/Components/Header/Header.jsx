4a04c7c86146339c8d50e433e9161a7b420d271f
Luis Ramiro Krupoviesa <krupoviesaramiro@gmail.com>
2021-12-27 23:48:50 -0300

    AJUSTE - Fix backoffice header (#170)
    * fix header styles
    
    * fix login button
    
    * fix styles
    
    * fix links hover
    
    * add backoffice header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,80 @@
 import React, { useState } from "react";
-import Menu from "../../assets/images/menu.svg";
+import { Link, NavLink } from "react-router-dom";
 import "../Header/header.scss";
-import { SideBar } from "./SideBar";
+import logo from "../../assets/images/logo.png";
+import { AuthLogout } from "./AuthLogout";
 
 const Header = () => {
-	const [open, setOpen] = useState(false);
-	return (
-		<>
-			<div className="headerLogo">
-				<button className="headerLogo__btn" onClick={() => setOpen(!open)}>
-					<img src={Menu} alt="Menu" className="headerLogo__img" />
-				</button>
-			</div>
-			<SideBar isOpen={open} />
-		</>
-	);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const showMenu = () => {
+    setMenuIsOpen((prev) => !prev);
+  };
+
+  const data = [
+    {
+      text: "Novedades",
+      link: "/backoffice/news",
+    },
+    {
+      text: "Actividades",
+      link: "/backoffice/activities",
+    },
+    {
+      text: "Categorías",
+      link: "/backoffice/categories",
+    },
+    {
+      text: "Testimonios",
+      link: "/backoffice/testimonials",
+    },
+    {
+      text: "Organización",
+      link: "/backoffice/organization",
+    },
+    {
+      text: "Slides",
+      link: "/backoffice/slides",
+    },
+    {
+      text: "Usuarios",
+      link: "/backoffice/users",
+    },
+    {
+      text: "Miembros",
+      link: "/backoffice/members",
+    },
+  ];
+
+  return (
+    <nav className="headerBackoffice header__container">
+      <Link to="/backoffice">
+        <img src={logo} alt="logo" width={130} className="header__logo" />
+      </Link>
+
+      <button
+        className="header__menuBtn form__btn-secondary"
+        onClick={showMenu}
+      >
+        MENU
+      </button>
+      <div className={`header__menuPanel ${menuIsOpen && "active"}`}>
+        <ul className="header__navbar">
+          {data.map((item, index) => (
+            <li key={index}>
+              <NavLink to={item.link} exact className="header__link">
+                {item.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        <div className="header__btnContainer">
+          <AuthLogout />
+        </div>
+      </div>
+      <i className="fas fa-pen pencilIcon"></i>
+    </nav>
+  );
 };
 export default Header;
--- src/Components/HomeDashboard/HomeDashboard.jsx
+++ src/Components/HomeDashboard/HomeDashboard.jsx
@@ -1,26 +1,29 @@
 import HomeCards from "../HomeCards/HomeCards";
-import "./HomeDashboard.scss"
+import "./HomeDashboard.scss";
 import { useState } from "react";
 import Header from "../Header/Header";
 
 const HomeDashboard = () => {
+  const cardsInfo = require("../../lib/mock/BackofficeDashboard.json");
+  const [usuario, setUsuario] = useState("");
 
-    const  cardsInfo  = require("../../lib/mock/BackofficeDashboard.json");
-    const [usuario, setUsuario] = useState("");
+  return (
+    <>
+      <h1 className="text-center">Bienvenido/a {usuario}</h1>
+      <div className="dashboard-grid my-5">
+        {cardsInfo.map((card, index) => {
+          return (
+            <HomeCards
+              title={card.title}
+              image={card.image}
+              key={index}
+              description={card.description}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Header />
-            <h1 className="text-center">Bienvenido/a {usuario}</h1>
-            <div className="dashboard-grid my-5">
-                {cardsInfo.map((card,index) =>{
-                    return <HomeCards title={card.title} image={card.image} key={index} description={card.description}/>
-                })}
-            </div>
-        </>
-
-    )
-
-}
-
-export default HomeDashboard
+export default HomeDashboard;

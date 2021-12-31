fdf2c83c9e5fb3d216ab74af6a41ef9be37639b8
jkominovic <70329467+JulianKominovic@users.noreply.github.com>
2021-12-31 12:43:49 -0300

    AJUSTE - 404 fix image (#182)
    * remove: redux boilerplate and redux
    
    * done: functional component
    
    * api: modify category
    
    * working component
    
    * image to base64 helper
    
    * fix: image received from object
    
    * fix: returns message
    
    * component ready to refactor
    
    * testing errors, verify
    
    * component done, waiting for title component
    
    * component done, waiting for title component
    
    * component refactor
    
    * libs: cypress and jest
    
    * fix: ckeditor bugs
    
    * refactor done
    
    * merge: OT103-28
    
    * Fix: bug in img logic
    
    * Console.log and tests removed
    
    * Console.log removed
    
    * libs: react-icons
    
    * spinner created
    
    * Feature: info button and spinner
    
    * console.logs removed
    
    * added exact router path
    
    * comments removed
    
    * created getActivityById from endpoint /activities/:id
    
    * added activities/:id route
    
    * added activity info component
    
    * documentation done
    
    * flex fixed
    
    * errors fixed
    
    * title disabled by the moment
    
    * main pulled
    
    * component NewsItemList done
    
    * route /backoffice/news  modified
    
    * component done, waiting for remove action
    
    * created news list
    
    * removed todo annotation
    
    * added styles
    
    * styles applied
    
    * styles created and applied
    
    * Delete yarn.lock
    
    * styles done and applied
    
    * catogories form styled
    
    * conflicting files commented
    
    * redux, redux-toolkit and redux-thunk installed
    
    * store created
    
    * conflict resolved
    
    * provider added
    
    * dev tool enabled
    
    * redux setup
    
    * Delete Activities.scss
    
    * react-redux added
    
    * reducers added to index
    
    * validateAuth created
    
    * authSlice created
    
    * authActions custom hooks created
    
    * dotenv package installed
    
    * removed dotenv
    
    * Deleted my previous styles file
    
    * Styles adjusted
    
    * Components adjusted
    
    * Mock componente deleted
    
    * utils changes
    
    * alerts added
    
    * skeletons added
    
    * little response fix
    
    * contact get alert error
    
    * contact form post alert error
    
    * Skeletons changed. Custom activity skeleton made
    
    * documentation activity skeleton
    
    * name fixed
    
    * timeout added
    
    * activity details centered
    
    * skeleton fixed
    
    * Added helper function
    
    * refactor
    
    * Merge main
    
    * auth features
    
    * ckeditor global fix
    
    * Login fixed
    
    * login form history push
    
    * Push state fixed
    
    * form Styles fixed
    
    * Image 404 changed
    
    * 404 image changed
    
    * Image 404 changeddd
    
    * lf to crlf
    
    Co-authored-by: Lucas <lucasmoauro.93@gmail.com>
    Co-authored-by: FlaviaCohen <flaviacohen1988@gmail.com>

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -3,7 +3,7 @@ import defaultImage from "../../assets/images/404.png";
 export const Title = ({ title, image }) => {
 	return (
 		<div>
-			<img src={!image ? defaultImage : image} alt="" />
+			<img src={!image ? defaultImage : image} alt="" className="image-300px" />
 			<h1 className="main-title">{title}</h1>
 		</div>
 	);

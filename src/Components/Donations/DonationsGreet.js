1812704f1e6da821bca5bf9a17ee8e1fe6b21471
Luis Ramiro Krupoviesa <krupoviesaramiro@gmail.com>
2022-01-04 11:01:20 -0300

    fix donate styles (#197)

--- src/Components/Donations/DonationsGreet.js
+++ src/Components/Donations/DonationsGreet.js
@@ -1,69 +1,65 @@
 import "./styles/donations.scss";
 import mercadoPago from "../../assets/mercado-pago.svg";
+import Arrow1 from "../../assets/images/arrow-1.png";
+import Hand from "../../assets/images/hand-heart.png";
 
 const DonationsGreet = ({
 	title = "Se parte del cambio para las familias de La Cava",
 }) => {
 	return (
-		<div>
-			<h1 className="txt-center"> {title} </h1>
-			<div className="d-flex justify-content-around my-5">
-				<div>
-					<a
-						href="https://mpago.la/2RW1MWq"
-						className="donations-div-link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						$100
-						<img src={mercadoPago} />
-					</a>
-				</div>
-				<div>
-					<a
-						href="https://mpago.la/2CUYssg"
-						className="donations-div-link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						$500
-						<img src={mercadoPago} />
-					</a>
-				</div>
-				<div>
-					<a
-						href="https://mpago.la/24eQKk4"
-						className="donations-div-link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						$1000
-						<img src={mercadoPago} />
-					</a>
-				</div>
-				<div>
-					<a
-						href="https://mpago.la/2epKeEX"
-						className="donations-div-link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						$2000
-						<img src={mercadoPago} />
-					</a>
-				</div>
+		<div className="donar__container">
+			<h1 className="text__title-secondary"> {title} </h1>
+			<div className="donar__links">
+				<a
+					href="https://mpago.la/2RW1MWq"
+					className="donations-div-link"
+					target="_blank"
+					rel="noreferrer"
+				>
+					$100
+					<img src={mercadoPago} />
+				</a>
+				<a
+					href="https://mpago.la/2CUYssg"
+					className="donations-div-link"
+					target="_blank"
+					rel="noreferrer"
+				>
+					$500
+					<img src={mercadoPago} />
+				</a>
+				<a
+					href="https://mpago.la/24eQKk4"
+					className="donations-div-link"
+					target="_blank"
+					rel="noreferrer"
+				>
+					$1000
+					<img src={mercadoPago} />
+				</a>
+				<a
+					href="https://mpago.la/2epKeEX"
+					className="donations-div-link"
+					target="_blank"
+					rel="noreferrer"
+				>
+					$2000
+					<img src={mercadoPago} />
+				</a>
+				<a
+					href="https://mpago.la/1xtazSs"
+					className="donations-div-link"
+					target="_blank"
+					rel="noreferrer"
+				>
+					$5000
+					<img src={mercadoPago} />
+				</a>
+			</div>
 
-				<div>
-					<a
-						href="https://mpago.la/1xtazSs"
-						className="donations-div-link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						$5000
-						<img src={mercadoPago} />
-					</a>
-				</div>
+			<div className="donar__donateContainer">
+				<img src={Arrow1} alt="curved arrow" className="donar__arrowImage" />
+				<img src={Hand} alt="hand with a heart" className="donar__handImage" />
 			</div>
 		</div>
 	);
